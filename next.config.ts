c1d668791505ea3d3efce66a308889feba773b5c
Vladimir <pestov.web@yandex.ru>
2026-04-08 14:18:28 +0300

    fix: allow OAuth provider avatar hostnames in next/image

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./src/shared/config/request.ts");
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      // Яндекс аватары
+      { hostname: "avatars.yandex.net" },
+      // GitHub аватары
+      { hostname: "avatars.githubusercontent.com" },
+      // Google аватары
+      { hostname: "lh3.googleusercontent.com" },
+    ],
+  },
+};
 
 export default withNextIntl(nextConfig);
